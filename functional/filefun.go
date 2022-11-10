b79f032eef659978afbdb8cc2fe4175892358290
jumrusnaew <jumrusnaew.chai@gmail.com>
2022-11-10 09:00:17 +0700

    First commit

---
+++ functional/filefun.go
@@ -0,0 +1,47 @@
+package functional
+
+import (
+	"bufio"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func File2lines(filePath string) ([]string, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	return LinesFromReader(f)
+}
+
+func LinesFromReader(r io.Reader) ([]string, error) {
+	var lines []string
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return lines, nil
+}
+func InsertStringToFile(path, str string, index int) error {
+	lines, err := File2lines(path)
+	if err != nil {
+		return err
+	}
+
+	fileContent := ""
+	for i, line := range lines {
+		if i == index {
+			fileContent += str
+		}
+		fileContent += line
+		fileContent += "\n"
+	}
+
+	return ioutil.WriteFile(path, []byte(fileContent), 0644)
+}
---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type RecordOfTaxCrypto struct {
+	Activity string
+	Coin     string
+	Price    float64
+	Amount   float64
+}
+type recordOfSale struct {
+	line   int
+	amount float64
+	coin   string
+}
+
+func main() {
+	record, err := os.Open("record.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer record.Close()
+	scanner := bufio.NewScanner(record)
+	lines := []RecordOfTaxCrypto{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		field := strings.Split(line, " ")
+		price, _ := strconv.ParseFloat(field[2], 32)
+		am, _ := strconv.ParseFloat(field[3], 32)
+		lines = append(lines, RecordOfTaxCrypto{
+			Activity: field[0],
+			Coin:     field[1],
+			Price:    float64(price),
+			Amount:   float64(am),
+		})
+	}
+	fmt.Printf("transection : %v \n", len(lines))
+	fmt.Println("---------------------------------------------------------")
+	rowIndex := 0
+	SalPrice := 0.0
+	Profit := 0.0
+	histPrice := []recordOfSale{}
+	for _, row := range lines {
+		//fmt.Println(row)
+
+		if row.Activity == "S" {
+			aliasAmount := row.Amount
+			priceSale := 0.0
+			priceBuy := 0.0
+			for o := 0; o < len(histPrice); o += 1 {
+				if aliasAmount > 0 && histPrice[o].amount > 0 {
+					if histPrice[o].coin == row.Coin {
+						fmt.Println(histPrice[o])
+						fmt.Println(lines[histPrice[o].line])
+						fmt.Println("-------------------")
+						histPrice[o].amount -= aliasAmount
+						fmt.Println("histamount = ", histPrice[o].amount)
+						fmt.Println("alias = ", aliasAmount)
+						fmt.Println(":", (histPrice[o].amount * lines[histPrice[o].line].Price))
+						aliasAmount = 0
+
+						// if histPrice[o].amount >= aliasAmount {
+
+						// } else {
+
+						// }
+					}
+				}
+			}
+
+			Profit += (float64(priceSale) - priceBuy)
+			SalPrice += float64(priceSale)
+			fmt.Print("\nSale Amount :", aliasAmount)
+			fmt.Println("\nBalance :", histPrice)
+			fmt.Printf("%v Sale Price : %.2f Buy Price : %.2f Profit : %.2f \n", row.Coin, priceSale, priceBuy, Profit)
+			fmt.Println("---------------------------------------------------------")
+		} else {
+			histPrice = append(histPrice, recordOfSale{
+				line:   rowIndex,
+				amount: row.Amount,
+				coin:   row.Coin,
+			})
+		}
+		rowIndex += 1
+	}
+	fmt.Println(histPrice)
+	fmt.Println("Sale Total", fmt.Sprintf("%.2f", SalPrice))
+	fmt.Println("Profit Total", fmt.Sprintf("%.2f", Profit))
+
+	// fmt.Println(t)
+	// fmt.Println(string(t))
+	// reader := bufio.NewReader(os.Stdin)
+	// tex, _, _ := reader.ReadLine()
+	// fmt.Println(tex)
+	// fmt.Println(string(tex))
+}
